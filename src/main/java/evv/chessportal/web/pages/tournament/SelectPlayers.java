f4cb9925095163b4293ffab8e491941a2c36c779
ESDPC <ESDPC@DESKTOP-NH88UNJ.home>
2018-08-29 00:29:18 +0200

    Corrección de lista de checkboxes en formulario.

--- src/main/java/evv/chessportal/web/pages/tournament/SelectPlayers.java
+++ src/main/java/evv/chessportal/web/pages/tournament/SelectPlayers.java
@@ -1,19 +1,22 @@
 package evv.chessportal.web.pages.tournament;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.tapestry5.PersistenceConstants;
+import org.apache.tapestry5.ValueEncoder;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
 import evv.chessportal.model.tournament.Tournament;
 import evv.chessportal.model.tournamentservice.TournamentService;
 import evv.chessportal.model.userprofile.UserProfile;
 import evv.chessportal.model.userservice.UserService;
 import evv.chessportal.model.util.exceptions.InstanceNotFoundException;
 import evv.chessportal.web.pages.Index;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.tapestry5.PersistenceConstants;
-import org.apache.tapestry5.annotations.Persist;
-import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -46,28 +49,77 @@ public class SelectPlayers {
     @Persist
     private List<Long> idList;
 
+    @Property
+    private Long tournamentId;
+
+    @Property
+    private ValueEncoder<UserProfile> encoder = new ValueEncoder<UserProfile>() {
+        @Override
+        public UserProfile toValue(String clientValue) {
+            UserProfile up = new UserProfile();
+            up.setId(Long.parseLong(clientValue));
+            return up;
+        }
+
+        @Override
+        public String toClient(UserProfile value) {
+            return String.valueOf(value.getId());
+        }
+    };
+
     @Inject
     private TournamentService tournamentService;
 
     void onActivate(Long tournamentId) {
+        this.tournamentId = tournamentId;
+    }
+
+    Long onPassivate() {
+        return tournamentId;
+    }
+
+    void setupRender() {
+        if (idList == null) {
+            idList = new ArrayList<Long>();
+        }
         try {
             tournament = tournamentService.findTournament(tournamentId);
         } catch (InstanceNotFoundException ex) {
-            //TODO error page
+            // TODO error page
             Logger.getLogger(TournamentDetails.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-
-    void onPrepareForRender() {
         if (userList == null || userList.isEmpty()) {
             userList = userService.searchAll();
         }
     }
 
+    public boolean isSelected() {
+        return (idList.contains(user.getId()));
+    }
+
+    public void setSelected(boolean selected) {
+        if (selected) {
+            idList.add(user.getId());
+        }
+    }
+
+    void onPrepareForSubmit() {
+        if (idList == null) {
+            idList = new ArrayList<Long>();
+        }
+        try {
+            tournament = tournamentService.findTournament(tournamentId);
+        } catch (InstanceNotFoundException ex) {
+            // TODO error page
+            Logger.getLogger(TournamentDetails.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        userList = userService.searchAll();
+    }
+
     Object onSuccess() {
 //        userList =  new ArrayList<UserProfile>();
         if (idList.isEmpty()) {
-           return Index.class;
+            return Index.class;
         }
         return this;
     }
