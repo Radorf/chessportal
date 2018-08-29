d526d0c2cb2ff81edcfe7f31e3695925a671b572
Noe <>
2018-08-29 19:55:28 +0200

    borrado de searchTournaments

--- src/main/java/evv/chessportal/web/pages/tournament/Tournaments.java
+++ src/main/java/evv/chessportal/web/pages/tournament/Tournaments.java
@@ -5,10 +5,55 @@
  */
 package evv.chessportal.web.pages.tournament;
 
+import evv.chessportal.model.tournament.Tournament;
+import evv.chessportal.model.tournamentservice.TournamentService;
+import evv.chessportal.web.util.DateManagerUtil;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import org.apache.tapestry5.PersistenceConstants;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
 /**
  *
  * @author E_Villodas
  */
 public class Tournaments {
+    @Inject
+    TournamentService tournamentService;
     
+    @Property
+    private String searchKey;
+    
+    @Property    
+    @Persist(PersistenceConstants.FLASH)
+    private ArrayList<Tournament> tournamentList;
+    
+    @Property
+    private Tournament tournament;
+    
+    void onPrepareForRender() {
+        if (tournamentList==null || tournamentList.isEmpty()){
+            tournamentList = tournamentService.searchAll();
+        }
+    }
+    
+    public String getStartDate(){
+        return DateManagerUtil.printCalendarDate(tournament.getStartDate());
+    }
+
+ 
+    public String getEndDate(){
+        return DateManagerUtil.printCalendarDate(tournament.getEndDate());
+    }
+    
+    public String getStartEnrolmentDate(){
+        return DateManagerUtil.printCalendarDate(tournament.getStartEnrolmentDate());
+    }
+
+ 
+    public String getEndEnrolmentDate(){
+        return DateManagerUtil.printCalendarDate(tournament.getEndEnrolmentDate());
+    }
 }
